08afb2bf2d4016c98d9493c6131a886132727884
zrw9979 <zrw9979@gmail.com>
2018-09-19 16:47:45 +0800

    [init]

---
+++ sd/src/src/router/index.js
@@ -0,0 +1,13 @@
+import App from '../App.vue'
+import Artical from '../view/artical.vue'
+import Catalog from '../view/catalog.vue'
+import List from '../view/list.vue'
+import Home from '../view/home.vue'
+
+export default [
+  { path: '/', component: App },
+  { path: '/Artical', name: 'artical', component: Artical },
+  { path: '/Catalog', name: 'catalog', component: Catalog },
+  { path: '/List', name: 'list', component: List },
+  { path: '/Home', name: 'home', component: Home }
+];
